d2cb99120393444cd3d158db9f5db15db87e72c5
Anwar Khan <akhan700010@gmail.com>
2018-04-10 17:17:36 +0100

    test profile REg

--- addUser.php
+++ addUser.php
@@ -1,12 +1,11 @@
 <?php
 $mongoClient = new MongoClient();
 $db = $mongoClient->ecommerce;
-$collection = $db->products;
+$collection = $db->clients;
 
 $fullName = filter_input(INPUT_POST, 'fullName', FILTER_SANITIZE_STRING);
 $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
-$password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_NUMBER_FLOAT,
-FILTER_FLAG_ALLOW_FRACTION);
+$password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
 $address = filter_input(INPUT_POST, 'address', FILTER_SANITIZE_STRING);
 
 $dataArray = [
@@ -25,7 +24,7 @@ if($fullName == "" || $email == "" || $password == "" || $address == "" ){
 if($returnVal['ok']==1){
     echo 'Thank you ' . $fullName . " for Registering";
 }else {
-    echo 'Error adding product';
+    echo 'Error creating account';
 }
 
 $mongoClient->close();

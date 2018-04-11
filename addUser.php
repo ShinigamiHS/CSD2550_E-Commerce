fffc7b3ab6e9008e5219beed3533c84547284878
Claudio Camilo <36632108+ShinigamiHS@users.noreply.github.com>
2018-04-11 16:08:25 +0100

    almost finished with login, missing checkbox

--- addUser.php
+++ addUser.php
@@ -1,32 +1,32 @@
 <?php
-$mongoClient = new MongoClient();
-$db = $mongoClient->ecommerce;
-$collection = $db->clients;
+    $mongoClient = new MongoClient();
+    $db = $mongoClient->ecommerce;
+    $collection = $db->customers;
 
-$fullName = filter_input(INPUT_POST, 'fullName', FILTER_SANITIZE_STRING);
-$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
-$password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
-$address = filter_input(INPUT_POST, 'address', FILTER_SANITIZE_STRING);
+    $fullName = filter_input(INPUT_POST, 'fullName', FILTER_SANITIZE_STRING);
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
+    $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+    $address = filter_input(INPUT_POST, 'address', FILTER_SANITIZE_STRING);
 
-$dataArray = [
-    "full name" => $fullName,
-    "email" => $email,
-    "password" => $password,
-    "address" => $address,
-    "past orders" => []
-];
-if($fullName == "" || $email == "" || $password == "" || $address == "" ){
-    echo 'Some fields might be missing and/or wrong.';
-    exit();
-} else {
-    $returnVal = $collection->insert($dataArray);
-}
+    $dataArray = [
+        "full name" => $fullName,
+        "email" => $email,
+        "password" => $password,
+        "address" => $address,
+        "past orders" => []
+    ];
+    if($fullName == "" || $email == "" || $password == "" || $address == "" ){
+        echo 'Some fields might be missing and/or wrong.';
+        exit();
+    } else {
+        $returnVal = $collection->insert($dataArray);
+    }
 
-if($returnVal['ok']==1){
-    echo 'Thank you ' . $fullName . " for registering";
-}else {
-    echo 'Error creating account';
-}
+    if($returnVal['ok']==1){
+        echo 'Thank you ' . $fullName . " for registering";
+    }else {
+        echo 'Error creating account';
+    }
 
-$mongoClient->close();
+    $mongoClient->close();
  ?>
